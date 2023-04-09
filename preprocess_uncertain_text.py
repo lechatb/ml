3819cb5e5f64c82f6a188c8c17b39a01160ac07a
lechatb <borovko.alex@gmail.com>
2023-04-09 19:36:25 +0300

    Работает по первой прикидке. Надо продолжать.

--- preprocess_uncertain_text.py
+++ preprocess_uncertain_text.py
@@ -1,5 +1,12 @@
+import re
+import string
+import timeit
+import numpy as np
+from numba import njit, int32, types, jit, prange
 from fuzzywuzzy import process, fuzz
 from zipfile import ZipFile
+import numba as nb
+
 
 def load_dictionary(zipname='unigrams.cyr.lc.zip', filename='unigrams.cyr.lc'):
     correct_words = set()
@@ -15,31 +22,30 @@ def load_dictionary(zipname='unigrams.cyr.lc.zip', filename='unigrams.cyr.lc'):
     except:
         return list()
 
+
 # Задаем список правильных слов
-correct_words = load_dictionary()
+correct_words = np.array(load_dictionary())
 
 
-def correct_text(text, dictionary = correct_words):
+def correct_text(text, dictionary=correct_words):
     """
     Corrects a given text string using a dictionary of valid words.
     """
     # Split text into individual words
     words = text.split()
-    
+
     # Loop over each word and try to match to a word in the dictionary
     for i, word in enumerate(words):
-        match_scores = [(fuzz.ratio(word, dict_word), dict_word) for dict_word in dictionary]
+        match_scores = [(fuzz.ratio(word, dict_word), dict_word)
+                        for dict_word in dictionary]
         best_match = max(match_scores, key=lambda x: x[0])
-        
+
         # If the best match score is above threshold, replace the word with the dictionary word
         if best_match[0] >= 70:
-            words[i] = best_match[1] 
+            words[i] = best_match[1]
 
     return " ".join(words)
 
-
-from numba import njit, int32, types, prange
-import numpy as np
 
 def edit_distance(s, t):
     n = len(s)
@@ -63,14 +69,16 @@ def edit_distance(s, t):
             else:
                 cost = 1
 
-            d[i][j] = min(d[i - 1][j] + 1, # Deletion
-                          d[i][j - 1] + 1, # Insertion
-                          d[i - 1][j - 1] + cost) # Substitution
+            d[i][j] = min(d[i - 1][j] + 1,  # Deletion
+                          d[i][j - 1] + 1,  # Insertion
+                          d[i - 1][j - 1] + cost)  # Substitution
 
     return d[n][m]
 
-@njit(int32(types.unicode_type, types.unicode_type))
-def edit_distance_numba(s, t):
+
+#@njit(int32(types.unicode_type, types.unicode_type))
+@jit
+def edit_distance_numba(s: types.string, t:types.string):
     n = len(s)
     m = len(t)
 
@@ -92,43 +100,77 @@ def edit_distance_numba(s, t):
             else:
                 cost = 1
 
-            d[i][j] = min(d[i - 1][j] + 1, # Deletion
-                          d[i][j - 1] + 1, # Insertion
-                          d[i - 1][j - 1] + cost) # Substitution
+            d[i][j] = min(d[i - 1][j] + 1,  # Deletion
+                          d[i][j - 1] + 1,  # Insertion
+                          d[i - 1][j - 1] + cost)  # Substitution
 
     return d[n][m]
 
-import re
+#@nb.njit(nb.types.Tuple((nb.types.int32, nb.types.List(nb.types.unicode_type)))(nb.types.unicode_type))
+#@nb.njit(nogil=True, cache=False)
+def check_word(word: str) -> tuple:
+    global correct_words
+    dictionary=correct_words
 
-def process_text(text):
-    words = re.findall(r'\b\w+\b', text)
-    for i, word in enumerate(words):
-        if i < len(words) - 1 and re.match(r'\w+-$', word) and re.match(r'^\w+', words[i+1]):
-            # hyphenated word broken across lines
-            word = word[:-1] + words[i+1]
-            words[i+1] = ''
-        Fxx(word)
+    dist = list(map(lambda x: edit_distance_numba(word, x), dictionary))
+    dist = np.asarray(dist)
+    min_dist=np.min(dist)
+    res = dictionary[np.argmin(dist)]
+    return min_dist, res
+
 
 #print(correct_text('формации © временном испопнении обязанностей я китронёра Ирафучастчикы эынка'))
 print(edit_distance_numba('городок', 'гоoрдок'))
 
-import timeit
 
 print(timeit.timeit(lambda: edit_distance_numba('городокккк', 'гоoрдокккк'), number=10000))
 print(timeit.timeit(lambda: edit_distance('городокккк', 'гоoрдок'), number=10000))
 
-import string
-
-def correct_text(input_string):
+@njit
+def correct_text(input_string, fix_hyphenation=True):
     def split_string(string):
         return re.findall(r"[\w']+|[ -.,!?;]", string)
-    
+
+    def find_word_left(arr):
+        for i in range(len(arr)):
+            if isinstance(arr[i], str) and arr[i].isalpha():
+                return i
+        return -1  # если такой ячейки не найдено
+
+    def find_word_right(arr):
+        for i in range(-1, -len(arr), -1):
+            if isinstance(arr[i], str) and arr[i].isalpha():
+                return i
+        return -1  # если такой ячейки не найдено
+
     result_string = (input_string.split('''\n'''))
-    result_string = list(map( split_string, result_string))
-    
+    result_string = list(map(split_string, result_string))
+    if fix_hyphenation:
+        for i in range(len(result_string)-1):
+            i1 = find_word_right(result_string[i])
+            i2 = find_word_left(result_string[i+1])
+            if i1 == -1 or i2 == -1:
+                continue
+            w1 = result_string[i][i1]
+            w2 = result_string[i+1][i2]
+            w3 = w1+w2
+            a1=check_word(w1)
+            a2=check_word(w2)
+            a3=check_word(w3)
+            
+            if a1[0] <= a2[0] + a3[0]:
+                result_string[i][i1]=w3
+                result_string[i]=result_string[i][:i1+1]
+                result_string[i+1][i2]=result_string[i+1][i2+1:]
+
     return result_string
 
-s = '''о Бедном? Гусаре! Замолвите Слово-
-Ваш Муж Не Пускает Меня/ На Постой
+
+print( check_word('Дшм'))
+
+s = '''о Бедном? Гусаре! Замолвите Сло-
+во, Ваш Муж Не Пускает Меня/ На Постой
 '''
-print( correct_text( s))
+#print(correct_text(s))
+
+
